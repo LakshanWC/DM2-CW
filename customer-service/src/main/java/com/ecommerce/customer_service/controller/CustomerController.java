522a7be4799238c5301ad66853d759af6f1a7d6f
LakshanWC <lakshanwc@gmail.com>
2025-04-18 12:03:35 +0530

    updated customer-service
    added endpoint to select and filter items

--- customer-service/src/main/java/com/ecommerce/customer_service/controller/CustomerController.java
+++ customer-service/src/main/java/com/ecommerce/customer_service/controller/CustomerController.java
@@ -1,8 +1,27 @@
 package com.ecommerce.customer_service.controller;
-import org.springframework.web.bind.annotation.RestController;
+import com.ecommerce.customer_service.dto.ProductDTO;
+import com.ecommerce.customer_service.repository.CustomerRepository;
+import com.ecommerce.customer_service.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
+@RequestMapping("/customers")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping
+    public List<ProductDTO> getAllProducts(
+            @RequestParam (required = false) String category,
+            @RequestParam (required = false) Double price) {
+        return customerService.getAllProducts(category,price);
+    }
+
 
 }
