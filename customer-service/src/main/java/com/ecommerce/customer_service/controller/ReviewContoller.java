8c6c18f0fd115c6cf69ed297c0e0f128f0e24f82
LakshanWC <lakshanwc@gmail.com>
2025-04-19 14:28:52 +0530

    connected mogoDB
    added endpoint to get all reviews by the product id

--- customer-service/src/main/java/com/ecommerce/customer_service/controller/ReviewContoller.java
+++ customer-service/src/main/java/com/ecommerce/customer_service/controller/ReviewContoller.java
@@ -1,4 +1,22 @@
 package com.ecommerce.customer_service.controller;
 
+import com.ecommerce.customer_service.model.Review;
+import com.ecommerce.customer_service.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reviews")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ReviewContoller {
+
+    @Autowired
+    private ReviewService reviewService;
+
+    @GetMapping("/{productId}")
+    public List<Review> findAllByProductId(@PathVariable int productId){
+        return reviewService.findAllByProductId(productId);
+    }
 }
